80fbe907f9f9a529f87d4156f88b2bea9079ac24
aikya <aikyaana004@gmail.com>
2024-05-06 22:56:31 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SportList from "./SportList";
+
+function App() {
+  return (
+    <div className="App">
+      <SportList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SportList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Sport from "./Sport"
+interface Props {
+}
+
+interface SportItem {
+  title: string
+}
+interface State {
+  sports: SportItem[];
+}
+class SportList extends React.Component<Props, State> {
+    constructor(props: Props) {
+      super(props);
+      this.state = {
+        sports: [{ title: "Pay rent" }, { title: "Submit assignment" }],
+      };
+    }
+    render() {
+      return (
+        <>
+          {this.state.sports.map((sport) => (
+            <Sport title={sport.title} />
+          ))}
+        </>
+      );
+    }
+  }
+export default SportList;
