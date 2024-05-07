9e02b7b76d24df124cae81a3d62a3d0564e2d1bd
aikya <aikyaana004@gmail.com>
2024-05-07 19:18:05 +0530

    Routing and API Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import SportList from "./SportList";
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import Notfound from './pages/Notfound';
+import ProtectedRoute from "./ProtectedRoute";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Signup />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/notfound",
+    element: <Notfound />,
+  },
+  {
+    path: "/articles",
+    element: <SportList />,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  }
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <SportList />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
