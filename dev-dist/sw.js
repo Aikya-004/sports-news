2ee19e32ddc1937cdd96fbeada9ec6b278f6af79
aikya <aikyaana004@gmail.com>
2024-05-19 14:11:58 +0530

    Icons are fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.579s65eobj"
+    "revision": "0.89dcoktaib"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
